895a871ad041febb497a945e585eb37bb9ea3569
Liam, SB Hoo <shibinhoo@gmail.com>
2023-11-12 16:23:54 +0100

    Clean up

--- tests/test_error_relay.py
+++ tests/test_error_relay.py
@@ -33,23 +33,3 @@ class TestErrorRelay(unittest.TestCase):
         decoded_error = ErrorRelay.try_decode_error_from_http_response(response)
         self.assertEqual(type(decoded_error), ValueError)
         self.assertEqual(str(decoded_error), "test error")
-
-    # def test_intercept_http_exception_on_http_exception_containing_value_error(self):
-    #     def raise_http_exception_containing_value_error():
-    #         raise ErrorRelay.encode_error_in_http_exception(ValueError())
-    #
-    #     @ErrorRelay.intercept_http_exception
-    #     def calling_func():
-    #         raise_http_exception_containing_value_error()
-    #
-    #     self.assertRaises(ValueError, calling_func)
-    #
-    # def test_intercept_http_exception_on_http_exception_not_containing_error(self):
-    #     def raise_http_exception_not_containing_error():
-    #         raise HTTPException(status_code=400)
-    #
-    #     @ErrorRelay.intercept_http_exception
-    #     def calling_func():
-    #         raise_http_exception_not_containing_error()
-    #
-    #     self.assertRaises(HTTPException, calling_func)
